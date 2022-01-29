6910be60f523259d7979772045e143f65b8632b0
bochain <bochainwu@gmail.com>
2022-01-29 16:39:46 +0800

    rel="noreferrer"

--- src/components/OnTheWeb.js
+++ src/components/OnTheWeb.js
@@ -16,10 +16,10 @@ const OnTheWeb = () =>{
         <>
         <h2>On the web</h2>
         <LinkList>
-            <Link><IoLogoGithub></IoLogoGithub><a href="https://github.com/bochainwu" target="_blank">  @bochainwu</a></Link>
-            <Link><IoLogoTwitter></IoLogoTwitter><a href="https://twitter.com/bochianwu" target="_blank">  @bochainwu</a></Link>
-            <Link><IoLogoInstagram></IoLogoInstagram><a href="https://twitter.com/bochianwu" target="_blank">  @bochainwu</a></Link>
-            <Link><IoMailOutline></IoMailOutline><a href="mailto:bochainwu@gmail.com" target="_blank"> Say Hello👋to me</a></Link>
+            <Link><IoLogoGithub></IoLogoGithub><a rel="noreferrer" href="https://github.com/bochainwu" target="_blank">  @bochainwu</a></Link>
+            <Link><IoLogoTwitter></IoLogoTwitter><a rel="noreferrer" href="https://twitter.com/bochianwu" target="_blank">  @bochainwu</a></Link>
+            <Link><IoLogoInstagram></IoLogoInstagram><a rel="noreferrer" href="https://twitter.com/bochianwu" target="_blank">  @bochainwu</a></Link>
+            <Link><IoMailOutline></IoMailOutline><a rel="noreferrer" href="mailto:bochainwu@gmail.com" target="_blank"> Say Hello👋to me</a></Link>
             
         </LinkList>
         </>
